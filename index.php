974914eec5739b626222eac6bf6587c8251f26fe
laurentfranchois <laurent.franchois23@gmail.com>
2017-01-05 17:24:44 +0100

    Modification fonctionnelle

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+include('includes/connexion.inc.php');
+include('includes/haut.inc.php');
+
+$id=0;
+$rep="";
+  if (isset($_GET['id']) && !empty($_GET['id'])){
+
+    $id=$_GET['id'];
+    $query = "select * from messages where id='".$_GET['id']."'";
+    $prep = $pdo->query($query);
+    if($data=$prep->fetch()){
+      $rep=$data['contenu'];
+    }
+  }
+?>
+
+<div class="row">
+    <form method="post" action="message.php">
+        <div class="col-sm-10">
+            <div class="form-group">
+                <textarea id="message" name="message" class="form-control" placeholder="Message"> <?php echo $rep;?></textarea>
+                <input type="hidden" name="id" value=<?php echo $id ?>>
+
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <button type="submit" class="btn btn-success btn-lg">Envoyer</button>
+        </div>
+    </form>
+</div>
+
+<?php
+$query = 'SELECT * FROM messages';
+$stmt = $pdo->query($query);
+
+while ($data = $stmt->fetch()) {
+	?>
+	<blockquote>
+		<?= $data['contenu']?>
+    <a href="index.php?id=<?=$data['id']?>"><button type="button" class="btn btn-success btn-lg">Modifier</button></a>
+
+	</blockquote>
+
+	<?php
+}
+?>
+
+<?php include('includes/bas.inc.php'); ?>
---
+++ message.php
@@ -0,0 +1,22 @@
+<?php
+include('includes/connexion.inc.php');
+
+if (isset($_POST['message']) && !empty($_POST['message'])) {
+	if(isset($_POST['id']) && !empty($_POST['id'])){
+		$query = 'UPDATE messages SET contenu = ? WHERE id = ?';
+		$prep = $pdo->prepare($query);
+		$prep->bindValue(1,$_POST['message']);
+		$prep->bindValue(2,$_POST['id']);
+	}
+	else{
+		$query = 'INSERT INTO messages (contenu) VALUES (?)';
+		$prep = $pdo->prepare($query);
+		$prep->bindValue(1,$_POST['message']);
+	}
+	$prep->execute();
+
+}
+
+header("Location:index.php");
+exit();
+?>
